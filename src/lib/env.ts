1f3fd80fa837ce1fcbabdc7c5ba6abeff4b40250
Suraj Roy <SURAJRAI1204@gmail.com>
2025-11-28 08:56:01 +0000

    Try fixing this error: `Runtime Error: ZodError: [ { "code": "invalid_ty

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -7,7 +7,7 @@ const envSchema = z.object({
 // We need to use process.env here to get the environment variables from the system.
 // The .env file is loaded automatically by Next.js.
 const envObject = {
-    HIANIME_API_BASE: process.env.HIANIME_API_BASE,
+    HIANIME_API_BASE: process.env.NEXT_PUBLIC_HIANIME_API_BASE,
 };
 
 // This will throw an error if the environment variables are missing or invalid.
