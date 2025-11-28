a058cabc43dcbed42eb1a6ab6d6f2d51d171b74e
Suraj Roy <SURAJRAI1204@gmail.com>
2025-11-28 09:00:44 +0000

    i have fixed the api url in env

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,13 +1,13 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  HIANIME_API_BASE: z.string().url(),
+  NEXT_PUBLIC_HIANIME_API_BASE: z.string().url(),
 });
 
 // We need to use process.env here to get the environment variables from the system.
 // The .env file is loaded automatically by Next.js.
 const envObject = {
-    HIANIME_API_BASE: process.env.NEXT_PUBLIC_HIANIME_API_BASE,
+    NEXT_PUBLIC_HIANIME_API_BASE: process.env.NEXT_PUBLIC_HIANIME_API_BASE,
 };
 
 // This will throw an error if the environment variables are missing or invalid.
