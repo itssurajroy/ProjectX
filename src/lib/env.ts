e9094d1b4871946737eee8bc24da0f9101fea22a
Suraj Roy <SURAJRAI1204@gmail.com>
2025-11-29 01:48:35 +0000

    NEXT_PUBLIC_HIANIME_API_BASE=https://aniwatch-api-five-dusky.vercel.app

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,16 +1,13 @@
-
 import { z } from 'zod';
 
 const envSchema = z.object({
-  NEXT_PUBLIC_HIANIME_API_BASE: z.string(),
+  NEXT_PUBLIC_HIANIME_API_BASE: z.string().url(),
 });
 
 // We need to use process.env here to get the environment variables from the system.
 // The .env file is loaded automatically by Next.js.
-// On the server, we can use the internal proxy URL. On the client, it will be undefined
-// and will use the relative path which will be handled by the Next.js proxy.
 const envObject = {
-    NEXT_PUBLIC_HIANIME_API_BASE: process.env.NEXT_PUBLIC_HIANIME_API_BASE || '/api',
+    NEXT_PUBLIC_HIANIME_API_BASE: process.env.NEXT_PUBLIC_HIANIME_API_BASE,
 };
 
 // This will throw an error if the environment variables are missing or invalid.
