fd7d107afeefd80137668a41d69cd116232f755b
Suraj Roy <SURAJRAI1204@gmail.com>
2025-11-30 17:55:22 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,7 +2,18 @@ import type { Metadata } from "next";
 import "../globals.css";
 import Providers from "../providers";
 import { Toaster as ShadToaster } from "@/components/ui/toaster";
+import { Lexend, Space_Grotesk } from "next/font/google";
+import { cn } from "@/lib/utils";
 
+const fontSans = Lexend({ 
+  subsets: ["latin"],
+  variable: '--font-primary',
+});
+
+const fontDisplay = Space_Grotesk({
+  subsets: ["latin"],
+  variable: '--font-display',
+});
 
 export const metadata: Metadata = {
   title: "ProjectX Admin",
@@ -19,7 +30,7 @@ export default function AdminRootLayout({
        <head>
          <meta httpEquiv="Strict-Transport-Security" content="max-age=31536000; includeSubDomains" />
        </head>
-      <body className="min-h-screen bg-background text-foreground antialiased">
+      <body className={cn("min-h-screen bg-background text-foreground font-sans antialiased", fontSans.variable, fontDisplay.variable)}>
         <Providers>
             <ShadToaster />
             {children}
