d498e66a5e52855f31d88493498dac7d0e213b8f
Suraj Roy <SURAJRAI1204@gmail.com>
2025-12-03 04:57:33 +0000

    Update this firebase config // Import the functions you need from the SD

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-8183683078-60ab0",
-  "appId": "1:20761769952:web:1a66600ebcca874bc81d85",
-  "apiKey": "AIzaSyBQSy1kHNRYvp2b-l0jnsUd44P-cXkJZDY",
-  "authDomain": "studio-8183683078-60ab0.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "20761769952"
+  apiKey: "AIzaSyBQSy1kHNRYvp2b-l0jnsUd44P-cXkJZDY",
+  authDomain: "studio-8183683078-60ab0.firebaseapp.com",
+  projectId: "studio-8183683078-60ab0",
+  storageBucket: "studio-8183683078-60ab0.appspot.com",
+  messagingSenderId: "20761769952",
+  appId: "1:20761769952:web:1a66600ebcca874bc81d85"
 };
