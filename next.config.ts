efffcfbe39c21b9f30bab7bdc12fdd93d592c25a
Suraj Roy <SURAJRAI1204@gmail.com>
2025-12-08 15:11:31 +0000

    Payment required
    OPTIMIZED_IMAGE_REQUEST_PAYMENT_REQUIRED
    
    fra1::57hwh-

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,6 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       { protocol: 'https', hostname: '**' },
     ],
